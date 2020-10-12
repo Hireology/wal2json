a63a5a045d9d96f1af1542a0bbdff41c64e8b4b8
Greig Wise <gwise@hireology.com>
2020-10-12 12:01:51 -0400

    test

--- ci_prep.sh
+++ ci_prep.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
-export PATH=/usr/lib/postgresql/${POSTGRESQL_VERSION}/bin/:$PATH
 
 cd $HOME/src/github.com/Hireology/wal2json
 USE_PGXS=1 PG_CONFIG=/usr/lib/postgresql/${POSTGRESQL_VERSION}/bin/pg_config make -e
 sudo USE_PGXS=1 PG_CONFIG=/usr/lib/postgresql/${POSTGRESQL_VERSION}/bin/pg_config make -e install
 
-psql -p ${POSTGRESQL_PORT} -c "alter system set wal_level = 'logical'"
-psql -p ${POSTGRESQL_PORT} -c "alter system set max_replication_slots = 1"
+psql -c "alter system set wal_level = 'logical'"
+psql -c "alter system set max_replication_slots = 1"
 sudo service postgresql restart
