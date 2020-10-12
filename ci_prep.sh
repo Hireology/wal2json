da698e197ba8b7dc07bf792bf3b5a629b81a3df6
Greig Wise <gwise@hireology.com>
2020-10-12 13:00:55 -0400

    merge_master: also update ci_script

--- ci_prep.sh
+++ ci_prep.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
+
 cd $HOME/src/github.com/Hireology/wal2json
-USE_PGXS=1 PG_CONFIG=/usr/lib/postgresql/10/bin/pg_config make -e
-sudo USE_PGXS=1 PG_CONFIG=/usr/lib/postgresql/10/bin/pg_config make -e install
+USE_PGXS=1 PG_CONFIG=/usr/lib/postgresql/${POSTGRESQL_VERSION}/bin/pg_config make -e
+sudo USE_PGXS=1 PG_CONFIG=/usr/lib/postgresql/${POSTGRESQL_VERSION}/bin/pg_config make -e install
 
 psql -c "alter system set wal_level = 'logical'"
-psql -c "alter system set max_replication_slots = 1"
+psql -c "alter system set max_replication_slots = 10"
 sudo service postgresql restart
-
